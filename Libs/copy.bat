146c5107597947926bc57c57ed1c22bb059482cd
YouCanAi <vm@vm.mba>
2024-01-15 02:45:40 +0800

    修复copy.bat的一处问题

--- Libs/copy.bat
+++ Libs/copy.bat
@@ -4,13 +4,18 @@
 set /p GameFolder=Game Folder:
 if "%GameFolder:~-1%" == "\" (
     set GameFolder="%GameFolder:~0,-1%"
+) else (
+    set GameFolder="%GameFolder%"
 )
 goto check
 
 :check
 set BepInExFolder=%GameFolder:~0,-1%\BepInEx\core"
 set LibraryFolder=%GameFolder:~0,-1%\Idle ShowOff_Data\Managed"
-goto copy
+if not exist %GameFolder% (goto folder_error)
+    if not exist %BepInExFolder% (goto folder_error)
+        if not exist %LibraryFolder% (goto folder_error)
+            goto copy
 
 :folder_error
 echo Wrong folder detected, check input
